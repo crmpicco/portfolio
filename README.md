46a9d54703f7adbd5a955900bf99708088783540
Craig R Morton <cmorton@reiss.co.uk>
2015-05-20 17:59:12 +0100

    Adding Swiftmailer to send contact emails to me via Gmail

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Craig R Morton - Web Developer
-www.crmpicco.co.uk basic web dev portfolio
+[www.crmpicco.co.uk](http://www.crmpicco.co.uk) basic web dev portfolio
+
+## Contact
+T: +44 (0)7811 574203
+
+E: crmpicco@aol.co.uk
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,45 @@
 <?php
+
+/**
+ * Contact Me - Mail script to send email to me with SwiftMailer
+ *
+ * @author Craig R Morton <crmpicco@aol.co.uk>
+ * @date   19-May-2015
+ */
+
+require_once '../swiftmailer/lib/swift_required.php';
+
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = $_POST['name'];
+if (empty($_POST['name']) ||
+    empty($_POST['email']) ||
+    empty($_POST['phone']) ||
+    empty($_POST['message']) ||
+    !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
+) {
+    echo "No arguments Provided!";
+
+    return false;
+}
+
+$name          = $_POST['name'];
 $email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-	
+$phone         = $_POST['phone'];
+$message       = $_POST['message'];
+
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+$email_body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+
+$transporter = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+    ->setUsername('ayrshireminiscontact@gmail.com')
+    ->setPassword('Gv7vhEoot6lqLS6hKbok');
+
+$mailer  = Swift_Mailer::newInstance($transporter);
+$message = Swift_Message::newInstance('Portfolio Enquiry')
+    ->setFrom(array('ayrshireminiscontact@gmail.com' => 'CRMPicco Portfolio Enquiry'))
+    ->setTo(array('crmpicco@aol.co.uk' => 'A name'))
+    ->setBody($email_body);
+
+$result = $mailer->send($message);
+
+return true;
 ?>
